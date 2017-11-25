70fb95c0fdca6b5b511ad079310a2e5d164275d4
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2017-11-25 14:28:20 +0100

    sudo

--- scripts/build-artifact.sh
+++ scripts/build-artifact.sh
@@ -5,6 +5,9 @@ printf "Create the artifact for commit $ARTIFACT_NAME\n"
 
 docker tag my_app mgonzalezbaile/my_app
 docker tag my_nginx mgonzalezbaile/my_nginx
+
+docker login -p $DOCKERHUB_PASSWORD -u $DOCKERHUB_USER
+
 docker push mgonzalezbaile/my_nginx
 docker push mgonzalezbaile/my_app
 
