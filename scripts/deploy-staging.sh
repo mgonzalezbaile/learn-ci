c9b113d7d82f04410ffc6f82ae4f2739ba6f602e
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2017-11-25 22:19:35 +0100

    sudo

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -10,7 +10,11 @@ curl -L https://github.com/docker/machine/releases/download/v0.13.0/docker-machi
 chmod +x /tmp/docker-machine &&
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
 
-docker-machine create --driver amazonec2  --amazonec2-open-port 80 --amazonec2-region us-west-1 learn-ci
-eval $(docker-machine env learn-ci)
-docker run -d --name php mgonzalezbaile/my_app
-docker run -pd 80:80 --link="php" my_nginx
+docker-machine create \
+  --driver generic \
+  --generic-ip-address=ec2-54-219-128-169.us-west-1.compute.amazonaws.com
+
+ eval $(docker-machine env learn-ci)
+
+docker-machine ssh learn-ci docker run -d --name php mgonzalezbaile/my_app
+docker-machine ssh learn-ci docker run -pd 80:80 --link="php" my_nginx
