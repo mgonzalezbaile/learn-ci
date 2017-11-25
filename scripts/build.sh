31132127dff2c92fd1d04f89f815589ecbf9bca8
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2017-11-25 14:15:55 +0100

    sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,4 @@ docker build -t my_nginx -f docker/nginx/Dockerfile .
 docker run my_app curl -s http://getcomposer.org/installer | php
 docker run -v $(pwd):/var/www/html my_app php composer.phar install
 
-chown -r www-data:www-data $(pwd)
+#chown -r www-data:www-data $(pwd)
