1599d4c9f65a3a17dc153eb01f2c4ba676072788
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2017-11-25 21:20:55 +0100

    sudo

--- scripts/deploy-staging.sh
+++ scripts/deploy-staging.sh
@@ -3,6 +3,7 @@
 set -ev
 printf "Deploy staging $ARTIFACT_NAME\n"
 
+mkdir ~/.aws/
 echo "aws_access_key_id = $AWS_ACCESS_KEY_ID\n aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
 
 eval $(docker-machine env learn-ci)
