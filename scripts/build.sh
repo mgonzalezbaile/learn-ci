eae6f2d34c1c856f3d1e15af81590acf2876b8e8
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2017-11-25 13:08:41 +0100

    sudo

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,8 @@
 
 set -ev
 
-sudo docker build -t my_nginx -f docker/nginx/Dockerfile .
-sudo docker build -t my_app -f docker/app/Dockerfile .
+docker build -t my_nginx -f docker/nginx/Dockerfile .
+docker build -t my_app -f docker/app/Dockerfile .
 
-sudo docker run --name php my_app curl -s http://getcomposer.org/installer | php
-sudo docker run --name php my_app php composer.phar install
+docker run --name php my_app curl -s http://getcomposer.org/installer | php
+docker run --name php my_app php composer.phar install
