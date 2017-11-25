ccd27508713ad96148bce2fe8eb04d0f0f8bde3f
mgonzalezbaile <m.gonzalezbaile@gmail.com>
2017-11-25 13:29:33 +0100

    sudo

--- scripts/run-php-tests.sh
+++ scripts/run-php-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -ev
-php bin/phpunit -c phpunit.xml.dist
-php bin/phpunit --coverage-clover=coverage.clover
-wget https://scrutinizer-ci.com/ocular.phar
-php ocular.phar code-coverage:upload --format=php-clover coverage.clover
+docker run my_app php bin/phpunit -c phpunit.xml.dist
+docker run my_app php bin/phpunit --coverage-clover=coverage.clover
+docker run my_app wget https://scrutinizer-ci.com/ocular.phar
+docker run my_app php ocular.phar code-coverage:upload --format=php-clover coverage.clover
